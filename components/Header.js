2e53bcbb9c41a35355c84ef6f6a91d7fbc18d790
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-13 09:38:27 +0600

    header fone

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../static/logo.png";
+
+const style = {
+
+    wrapper: "flex justify-center gap-10 p-5 bg-[#FCC017]",
+    content: "max-w-7xl flex-1 flex justify-between",
+    logoContainer: "flex items-center flex-start",
+    logo: "cursor-pointer object-contain",
+    bannerNav:"flex cursor-pointer space-x-5  items-center"
+
+};
+
+const Header = () => {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <div className={style.logoContainer}>
+          <Image className={style.logo} src={logo} height={40} width={200} />
+        </div>
+        <div className={style.bannerNav}>
+
+            <div>Our Story</div>
+            <div>Membership</div>
+            <div>Sign in</div>
+            <div>Get Started</div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
